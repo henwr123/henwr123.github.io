f6e6a8ea9825e6180e0e161c39e5feaf68bf795d
William Hendrick <william.hendrick@gmail.com>
2022-01-29 21:55:28 -0600

    Click to change the attributes

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+const rb1 = document.querySelector('input[id="cb-skills"')
+
+rb1.addEventListener('change', function(e) {
+    document.getElementById('card-experience').style.display = 'none'
+    document.getElementById('card-skills').style.display = 'block'
+})
+
+const rb2 = document.querySelector('input[id="cb-experience"')
+
+rb2.addEventListener('change', function(e) {
+    document.getElementById('card-experience').style.display = 'block'
+    document.getElementById('card-skills').style.display = 'none'
+})
+
+
+document.addEventListener('DOMContentLoaded',() => {
+    document.getElementById('cb-skills').click()
+})
